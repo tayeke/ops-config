3316f487ab1e265be485f06aacd2c4513021134e
Taylor J. Eke <tayloreke@gmail.com>
2014-09-24 21:18:58 -0700

    updated again

--- redis-config/recipes/generate.rb
+++ redis-config/recipes/generate.rb
@@ -1,19 +1,21 @@
-node[:deploy].each do |appname, deployconfig|
+node[:deploy].each do |app_name, deploy_config|
   # determine root folder of new app deployment
-  approot = "#{deployconfig[:deployto]}/current"
+  app_root = "#{deploy_config[:deploy_to]}/current"
 
-  # use template .redis.yml.erb. to generate 'config/redis.yml'
-  template "#{approot}/config/redis.yml" do
+  # use template 'redis.yml.erb' to generate 'config/redis.yml'
+  template "#{app_root}/config/redis.yml" do
     source "redis.yml.erb"
     cookbook "redis-config"
 
     # set mode, group and owner of generated file
     mode "0660"
-    group deployconfig[:group]
-    owner deployconfig[:user]
+    group deploy_config[:group]
+    owner deploy_config[:user]
 
-    # define variable .@redis. to be used in the ERB template
-    variables(:redis => deployconfig[:redis] || {})
+    # define variable “@redis” to be used in the ERB template
+    variables(
+      :redis => deploy_config[:redis] || {}
+    )
 
     # only generate a file if there is Redis configuration
     not_if do
